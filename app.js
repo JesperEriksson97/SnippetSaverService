b6284e2a57f08ad87233faa42b2fdd64441ae61c
je223gs <je223gs@student.lnu.se>
2020-02-18 14:46:35 +0100

    Simple express server up and running

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const express = require('express')
+const app = express()
 
+app.get('/', (req, res) => {
+    console.log("Hello World")
+    res.send('hello world')
+})
 
-console.log("Hello World")
+app.listen(8000, () => {
+    console.log('App is listening on port 8000')
+})
+
